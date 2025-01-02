ba869b59b9e9a65725b91683b76f21a0a46e836b
miriam <miriamibanezmunoz148@gmail.com>
2025-01-02 13:51:33 +0100

    creo objeto,añado elemento al dom y hago evento

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -2,23 +2,33 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+/*hago un objeto con los correos de los admi y sus contraseñas*/
+const users = {
+    "usuario1@gmail.com" : "contraseña1",
+    "usuario2@gmail.com" : "contraseña2",
+    "usuario3@gmail.com" : "contraseña3",
+    "usuario4@gmail.com" : "contraseña4",
+    "usuario5@gmail.com" : "contraseña5",
+    "usuario6@gmail.com" : "contraseña6"
+}
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+/*añado y creo elem en el dom*/
+const emailInput = document.getElementById("email")
+const passwordInput = document.getElementById("password")
+const loginButton = document.getElementsByClassName("btn")
 
-// setupCounter(document.querySelector('#counter'))
+const error = document.createElement("p")
+error.textContent = "Correo o contraseña incorrectos"
+error.style.display = "none"
+button.appendchild("error")
+
+/*evento click */
+loginButton.addEventListener("click", () => {
+    if (users[email] && users[email] === password) {
+        return "hola"
+    }
+    else {
+
+        error.style.display = "block"
+    }
+})
