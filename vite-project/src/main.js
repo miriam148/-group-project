63214d4989b8a70a9ee3784ddebaeb4d2723dd7c
miriam <miriamibanezmunoz148@gmail.com>
2025-01-02 18:33:23 +0100

    modifico el evento pero sigue sin funcionar completo y modifico un elemento del dom

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -15,20 +15,22 @@ const users = {
 /*añado y creo elem en el dom*/
 const emailInput = document.getElementById("email")
 const passwordInput = document.getElementById("password")
-const loginButton = document.getElementsByClassName("btn")
+const loginButton = document.querySelector(".btn")
 
 const error = document.createElement("p")
 error.textContent = "Correo o contraseña incorrectos"
 error.style.display = "none"
-button.appendchild("error")
+document.querySelector(".login-box").appendChild(error)
+
 
 /*evento click */
-loginButton.addEventListener("click", () => {
-    if (users[email] && users[email] === password) {
-        return "hola"
+loginButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    if (users[emailInput.ValueMax.trim()] === passwordInput.ValueMax.trim()) {
+    window.location.href = "otraPagina.html" 
     }
     else {
-
-        error.style.display = "block"
+       
+        alert("correo o contraseña incorrectos")
     }
 })
