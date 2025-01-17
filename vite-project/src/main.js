b5bb46f5f4207561d59880442e291678432a062c
jesus <jesusrc999@gmail.com>
2025-01-17 12:00:39 +0100

    add alertas

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -37,11 +37,21 @@ nombreUsuario.classList.add('nombre-usuario');
 nombreUsuario.textContent = 'Juan Pérez';
 
 const state = document.createElement('button');
-state.textContent = 'Activo/Inactivo';
+state.textContent = 'Inactivo';
 state.classList.add('state');
+
+const buttonMod = document.createElement('button');
+buttonMod.textContent = 'Modificar';
+buttonMod.classList.add('button-mod');
+
+const buttonDel = document.createElement('button');
+buttonDel.textContent = 'Eliminar';
+buttonDel.classList.add('button-del');
 
 usuario.appendChild(nombreUsuario);
 usuario.appendChild(state);
+usuario.appendChild(buttonMod);
+usuario.appendChild(buttonDel);
 userList.appendChild(usuario);
 containerAp6.appendChild(userList)
 
@@ -49,3 +59,43 @@ document.body.appendChild(containerAp6);
 }
 
 addElementAp6();
+
+//funcion llamada base de datos para recibir los nombres
+
+// funcion cree los contenedores de cada usuario, con datos concretos
+
+// boton activo/inactivo funcionallidad para que cambie el estado en la base de datos
+const stateButton = document.querySelector('.state');
+
+stateButton.addEventListener('click', () => {
+    if (stateButton.textContent === 'Inactivo') {
+        stateButton.textContent = 'Activo';
+        alert('El estado cambió')
+    } else {
+        stateButton.textContent = 'Inactivo';
+    }
+});
+
+stateButton.addEventListener( 'click', () => {
+    if (stateButton.classList.contains('state')) {
+        stateButton.classList.remove('state');
+        stateButton.classList.add('state-change');
+    } else {
+        stateButton.classList.remove('state-change');
+        stateButton.classList.add('state');
+    }
+})
+
+
+// añadir boton eliminar y modificar, crear alertas para cada uno
+const alertDelete = document.querySelector('.button-del');
+
+alertDelete.addEventListener('click', () =>
+{
+    alert('¿Está seguro de que quiere eliminar este usuario?')
+})
+
+// añadir funcion para que al hacer click en cada usuario se muestre una vista detalle
+
+
+// ejemplo funcion para unir con la parte de Ana G
