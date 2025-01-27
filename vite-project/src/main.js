d1f1a6fa09b7176ee9204e6363c0c61795551bac
Ana Molina <ana@gmail.com>
2025-01-27 16:51:34 +0100

    ultimas modificaciones

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -2,6 +2,12 @@ import "./style.css";
 
 const API_URL = "http://localhost:3000";
 
+//CONTENEDORES GENERALES
+// const app1 = document.querySelector("#app1");
+// const containerAp6 = document.querySelector("#containerAp6");
+// const containerUserNew = document.querySelector("#containerNewUser");
+// const containerDetails = document.querySelector("#containerDetails");
+
 
 // FUNCIONES DE BORRADO
 
@@ -18,37 +24,20 @@ const API_URL = "http://localhost:3000";
 //   container.innerHTML = "";
 // }
 
-//CONTENEDORES GENERALES
-// const app1 = document.querySelector("#app1");
-// const containerAp6 = document.querySelector("#containerAp6");
-// const containerUserNew = document.querySelector("#containerNewUser");
-// const containerDetails = document.querySelector("#containerDetails");
-
-
-
-//Funcion borrar contenedor de Nuevo Usuario
 function deleteContainerNewUser() {
   const containerNU = document.querySelector("#containerNewUser");
   containerNU.innerHTML = "";
 }
+
 function deleteAlert() {
   const container = document.querySelector("#alertaContainer");
   container.innerHTML = "";
 }
-//BOTONES
-
-//Botones Volver y cerrar sesion
-
-
-//Botones Cancelar y/o Aceptar
-
-
-//ALERTA
 
 function cerrarAlerta() {
   const containerAlerta = document.querySelector("#alertaContainer");
   if (containerAlerta) {
-    containerAlerta.style.display = "none"; // Ocultar la alerta
+    containerAlerta.style.display = "none";
   }
 }
 
@@ -64,14 +53,15 @@ function alerta() {
 
   const aviso = document.createElement("div");
   aviso.classList.add("NU-aviso");
-  aviso.textContent = "Al pulsar en Aceptar los datos no se guardarán ¿Desea continuar?";
+  aviso.textContent =
+    "Al pulsar en Aceptar los datos no se guardarán ¿Desea continuar?";
 
   const siBtn = document.createElement("button");
-  siBtn.classList = "bottomDetail";
+  siBtn.classList = "buttonDetail";
   siBtn.textContent = "Aceptar";
 
   const cancelBtn = document.createElement("button");
-  cancelBtn.classList = "bottomDetail";
+  cancelBtn.classList = "buttonDetail";
   cancelBtn.textContent = "Cancelar";
 
   aviso.appendChild(cancelBtn);
@@ -93,11 +83,153 @@ function alerta() {
   containerNewUser.appendChild(containerAlerta);
 }
 
-//SELECTORES
-// const selectorIdNewUser = document.querySelector("#selectorIdNewUser");
-// const selectorPassNewUser = document.querySelector("#selectorNewUserPass");
+function buttonSaveNU(){
+  const saveBtnNU = document.createElement("button");
+  saveBtnNU.classList = "NU-btnBack";
+  saveBtnNU.textContent = "Guardar";
+  saveBtnNU.addEventListener("click", async () => {
+    try {
+      const nameNewUserInput = document.querySelector("#nameNewUserInput").value;
+      const lastnameNewUserInput = document.querySelector("#lastnameNewUserInput").value;
+      const phoneNumberNewUserInput = document.querySelector("#phoneNumberNewUserInput").value;
+      const roadNewUserInput = document.querySelector("#roadNewUserInput").value;
+      const postCodeNewUserInput = document.querySelector("#postCodeNewUserInput").value;
+      const cityNewUserInput = document.querySelector("#cityNewUserInput").value;
+      const emailNewUserInput = document.querySelector("#emailNewUserInput").value;
+      const passwordNewUserInput = document.querySelector("#passwordNewUserInput").value;
+      const subscriptionNewUserInput = document.querySelector("#subscriptionNewUserInput").value;
+      const roleNewUserInput = document.querySelector("#roleNewUserInput").value;
 
-//CREACIÓN DE LOS ELEMENTOS PARA LA FUNCION newUserCreate
+      if (
+        !nameNewUserInput ||
+        !lastnameNewUserInput ||
+        !phoneNumberNewUserInput ||
+        !roadNewUserInput ||
+        !postCodeNewUserInput ||
+        !cityNewUserInput ||
+        !emailNewUserInput ||
+        !passwordNewUserInput
+      ) {
+        alert("Por favor, complete todos los campos.");
+        return;
+      }
+
+      const phoneRegex = /^[0-9]{9}$/;
+      if (!phoneRegex.test(phoneNumberNewUserInput)) {
+        alert("El número de teléfono debe contener exactamente 9 dígitos.");
+        return;
+      }
+
+      const postCodeRegex = /^[0-9]{5}$/;
+      if (!postCodeRegex.test(postCodeNewUserInput)) {
+        alert("El código postal debe contener exactamente 5 dígitos.");
+        return;
+      }
+
+      const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+      if (!emailRegex.test(emailNewUserInput)) {
+        alert("El formato del email es inválido.");
+        return;
+      }
+
+      const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+      if (!passwordRegex.test(passwordNewUserInput)) {
+        alert("La contraseña debe tener al menos 8 caracteres, una letra mayúscula, un número y un carácter especial.");
+        return;
+      }
+      
+      const users = {
+        name: nameNewUserInput,
+        lastname: lastnameNewUserInput,
+        phoneNumber: phoneNumberNewUserInput,
+        road: roadNewUserInput,
+        postCode: postCodeNewUserInput,
+        city: cityNewUserInput,
+        email: emailNewUserInput,
+        password: passwordNewUserInput,
+        subscription: subscriptionNewUserInput,
+        role: roleNewUserInput,
+      };
+      const url = `${API_URL}/api/signup`;
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(users),
+      });
+
+      if (response.ok) {
+        alert("Usuario creado");
+        deleteContainerNewUser();
+        newUserCreate();
+      } else {
+        alert("El email o contraseña ya existe en nuestra base de datos");
+      }
+    } catch (error) {
+      alert("Error al crear usuario");
+      console.error(error);
+    }
+  });
+
+  return saveBtnNU
+}
+
+function buttonCancelNU(){
+ const cancelBtnNU = document.createElement("button");
+  cancelBtnNU.classList = "NU-btnBack";
+  cancelBtnNU.textContent = "Cancelar";
+
+  cancelBtnNU.addEventListener("click", () => {
+    setTimeout(() => {
+      alerta();
+    }, 1000);
+    deleteAlert();
+  });
+return cancelBtnNU
+}
+
+function buttonsActionNewUser() {
+  const commandsActionNU = document.createElement("div");
+  commandsActionNU.classList = "NU-actions";
+  commandsActionNU.appendChild(buttonSaveNU());
+  commandsActionNU.appendChild(buttonCancelNU());
+  return commandsActionNU;
+}
+
+function buttonBackNU(){
+  const backBtnNU = document.createElement("button");
+  backBtnNU.classList = "NU-btnBack";
+  backBtnNU.textContent = "Volver";
+
+  backBtnNU.addEventListener("click", () => {
+    deleteContainerNewUser();
+    containerUserNew.style.display = "none";
+    containerAp6.style.display = "block";
+    addElementAp6();
+  });
+  return backBtnNU
+}
+
+function buttonCloseNU(){
+const closeBtnNU = document.createElement("button");
+  closeBtnNU.classList = "NU-btnBack";
+  closeBtnNU.textContent = "Cerrar Sesión";
+  closeBtnNU.addEventListener("click", () => {
+    deleteContainerNewUser();
+    containerUserNew.style.display = "none";
+    app1.style.display = "block";
+    loginAdm();
+  });
+  return closeBtnNU
+}
+
+function buttonsBackNewUser() {
+  const commandsBackNU = document.createElement("div");
+  commandsBackNU.classList = "NU-commands";
+
+  commandsBackNU.appendChild(buttonBackNU());
+  commandsBackNU.appendChild(buttonCloseNU());
+  return commandsBackNU;
+}
 
 function nameNewUser() {
   const nameNU = document.createElement("input");
@@ -106,6 +238,7 @@ function nameNewUser() {
   nameNU.placeholder = "Nombre";
   return nameNU;
 }
+
 function lastnameNewUser() {
   const lastnameNU = document.createElement("input");
   lastnameNU.id = "lastnameNewUserInput";
@@ -174,15 +307,11 @@ function roleNewUser() {
   const roleNU = document.createElement("input");
   roleNU.id = "roleNewUserInput";
   roleNU.classList = "NU-title";
- roleNU.placeholder = "user/admin";
+  roleNU.placeholder = "user/admin";
   return roleNU;
 }
 
-
-
- //Plantilla creación Nuevo Usuario
 function newUserCreate() {
-  deleteContainerNewUser()
   const containNewUser = document.createElement("div");
 
   const elementsNewUser = document.createElement("div");
@@ -202,10 +331,6 @@ function newUserCreate() {
   elementsNewUser.appendChild(passwordNewUser());
   elementsNewUser.appendChild(subscriptionNewUser());
   elementsNewUser.appendChild(roleNewUser());
-  // elementsNewUser.appendChild(selectorIdNewUser);
-  // elementsNewUser.appendChild(selectorPassNewUser);
-
-
   elementsNewUser.appendChild(buttonsActionNewUser());
 
   containNewUser.appendChild(buttonsBackNewUser());
@@ -219,120 +344,4 @@ function newUserCreate() {
   app.appendChild(containerNU);
 }
 
-
-function buttonsActionNewUser() {
-  const commandsActionNU = document.createElement("div");
-  commandsActionNU.classList = "NU-actions";
-
-  const saveBtnNU = document.createElement("button");
-  saveBtnNU.classList = "NU-btnBack";
-  saveBtnNU.textContent = "Guardar";
-  saveBtnNU.addEventListener("click", async () => {
-    try {
-      const nameNewUserInput = document.querySelector("#nameNewUserInput").value;
-      const lastnameNewUserInput = document.querySelector("#lastnameNewUserInput").value;
-      const phoneNumberNewUserInput = document.querySelector("#phoneNumberNewUserInput").value;
-      const roadNewUserInput = document.querySelector("#roadNewUserInput").value;
-      const postCodeNewUserInput = document.querySelector("#postCodeNewUserInput").value;
-      const cityNewUserInput = document.querySelector("#cityNewUserInput").value;
-      const emailNewUserInput = document.querySelector("#emailNewUserInput").value;
-      const passwordNewUserInput = document.querySelector("#passwordNewUserInput").value;
-      const subscriptionNewUserInput = document.querySelector("#subscriptionNewUserInput").value;
-      console.log('subscription:', subscriptionNewUserInput);  // Agrega un log para verificar el valor
-
-      const roleNewUserInput = document.querySelector("#roleNewUserInput").value;
-console.log('role:', roleNewUserInput);
-
-      if (!nameNewUserInput || !lastnameNewUserInput || !phoneNumberNewUserInput || !roadNewUserInput || !postCodeNewUserInput || !cityNewUserInput || !emailNewUserInput || !passwordNewUserInput) {
-        alert("Por favor, complete todos los campos.");
-        return;
-      }
-
-      const users = {
-        name: nameNewUserInput,
-        lastname: lastnameNewUserInput,
-         phoneNumber: phoneNumberNewUserInput,
-        road: roadNewUserInput,
-        postCode: postCodeNewUserInput,
-        city: cityNewUserInput,
-        email: emailNewUserInput,
-        password: passwordNewUserInput,
-        subscription: subscriptionNewUserInput,
-        role: roleNewUserInput,
-      };
-      console.log(users)
-      // if (!users.name || !users.lastname || !users.phoneNumber || !users.road || !users.postCode || !users.city  || !users.email || !users.password) {
-      //   alert("Por favor, complete todos los campos.");
-      //   return;
-      // }
-      const url = `${API_URL}/api/signup`; 
-         const response = await fetch(url, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(users), 
-      });
-
-      if (response.ok) {
-        alert("Usuario creado");
-        deleteContainerNewUser();
-        newUserCreate()
-      } else {
-        alert("El email o contraseña ya existe en nuestra base de datos");
-      }
-    } catch (error) {
-      alert("Error al crear usuario");
-      console.error(error); 
-    }
-  });
-
-  const cancelBtnNU = document.createElement("button");
-  cancelBtnNU.classList = "NU-btnBack";
-  cancelBtnNU.textContent = "Cancelar";
-
-  cancelBtnNU.addEventListener("click", () => {
-    setTimeout(() => {
-      alerta();
-    }, 1000);
-    deleteAlert();
-  });
-
-  commandsActionNU.appendChild(saveBtnNU);
-  commandsActionNU.appendChild(cancelBtnNU);
-  return commandsActionNU;
-}
-
 newUserCreate();
-
-function buttonsBackNewUser() {
-  const commandsBackNU = document.createElement("div");
-  commandsBackNU.classList = "NU-commands";
-
-  const backBtnNU = document.createElement("button");
-  backBtnNU.classList = "NU-btnBack";
-  backBtnNU.textContent = "Volver";
-
-  backBtnNU.addEventListener("click", () => {
-    deleteContainerNewUser();
-    containerUserNew.style.display = "none";
-    containerAp6.style.display = "block";
-    addElementAp6();
-  });
-
-  const closeBtnNU = document.createElement("button");
-  closeBtnNU.classList = "NU-btnBack";
-  closeBtnNU.textContent = "Cerrar Sesión";
-  closeBtnNU.addEventListener("click", () => {
-    deleteContainerNewUser();
-    containerUserNew.style.display = "none";
-    app1.style.display = "block";
-    loginAdm();
-  });
-
-  commandsBackNU.appendChild(backBtnNU);
-  commandsBackNU.appendChild(closeBtnNU);
-  return commandsBackNU;
-}
-
-
-
-
